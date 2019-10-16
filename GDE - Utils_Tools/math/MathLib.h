663fa7e809c55070be505c86dbf26126e951f843
mecha-rm <43918914+mecha-rm@users.noreply.github.com>
2019-10-16 08:04:57 -0400

    Updating methods and includes
    Updating methods and includes

--- GDE - Utils_Tools/math/MathLib.h
+++ GDE - Utils_Tools/math/MathLib.h
@@ -6,12 +6,12 @@
 #ifndef MATH_LIBRARY
 #define MATH_LIBRARY
 
-#include "Collisions.h"
+#include "Collision.h"
 #include "LargeInt.h"
 #include "Matrix.h"
 #include "Quaternion.h"
 #include "Rotation.h"
-#include "Shape.h"
+#include "Object.h"
 #include "Vector.h"
 
 #endif //!MATH_LIBRARY
