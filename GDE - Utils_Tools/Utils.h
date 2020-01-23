e6d3e4ae5fa67962dfc88eb0431a81e2925edc17
mecha-rm <43918914+mecha-rm@users.noreply.github.com>
2020-01-22 22:00:46 -0500

    Clamp and fileExists
    Functions to check if a file exists and to clamp values.

--- GDE - Utils_Tools/Utils.h
+++ GDE - Utils_Tools/Utils.h
@@ -4,6 +4,7 @@
 
 #include "math/Matrix.h"
 #include <string>
+#include <sstream>
 
 namespace util
 {
@@ -26,6 +27,34 @@ namespace util
 	// ignoreCase: if false, then the function is case sensitive. If true, then the function ignores cases when looking for oldSubstr.
 	std::string replaceSubstring(std::string str, std::string oldSubstr, std::string newSubstr, bool ignoreCase = false);
 
+	// splits the string into a vector of the provided data type using spaces.
+	template<typename T>
+	const std::vector<T> splitString(std::string str)
+	{
+		std::stringstream ss; // the string stream.
+		std::vector<T> vec; // the vector used for the vertex.
+		T var; // used to store the item from the string.
+
+
+		// if the string is of length 0, then an empty vector is returned.
+		if (str.length() == 0)
+			return std::vector<T>();
+
+		ss.str(str); // stores the string in the stream
+
+		while (ss >> var) // while the string stream isn't empty
+		{
+			// if the conversion failed, the string stream moves onto the next item.
+			if (ss.bad())
+				continue;
+
+			vec.push_back(var); // saves in the vector
+		}
+
+		return vec; // returns the vector
+	}
+
+
 	// checks to see if a string is an integer; this is improved from isNum.
 	bool isInt(std::string str);
 
@@ -35,30 +64,65 @@ namespace util
 	// checks if a string is a number. IsInt or IsDecimal should be used instead.
 	bool isNum(std::string str);
 
+	// convets the string to the provided data type. Do note that the value might be truncated if the conversion doesn't make logical sense.
 	template<typename T>
-	void addToVector(std::vector<T *> & vector, T * val) // adds an element to a pointer vector if it isn't already in there. Because this is a template, the definition is placed here.
+	T convertString(const std::string & str)
 	{
-		for (T * item : vector) // if the vector already contains the pointer, it is not added.
+		// TODO: do error checking for this function.
+		std::stringstream ss; // the string stream.
+		T val; // used to store the item from the string.
+
+		ss.str(str); // stores the string in the stream
+
+		ss >> val;
+
+		return val;
+	}
+
+	// returns 'true' if a file can be opened successsfully, and false if it cannot.
+	bool fileAccessible(const std::string & filePath);
+
+	// returns the minimum between two values.
+	// the less than operator (<) must be overloaded for this function to work.
+	template<typename T>
+	T min(T val1, T val2) { return (val1 < val2) ? val1 : val2; }
+
+	// returns the maximum between two values.
+	// the greater than operator (>) must be overloaded for this function to work.
+	template<typename T>
+	T max(T val1, T val2) { return (val1 > val2) ? val1 : val2; }
+
+	// caps 'val' using lowerBound and upperBound. The (<) and (>) operators must be overloaded for this function. 
+	template<typename T>
+	T clamp(T val, T lowerBound, T upperBound) { return (val < lowerBound) ? lowerBound : (val > upperBound) ? upperBound : val; };
+
+	template<typename T>
+	bool addToVector(std::vector<T*>& vector, T* val) // adds an element to a pointer vector if it isn't already in there. Because this is a template, the definition is placed here.
+	{
+		for (T* item : vector) // if the vector already contains the pointer, it is not added.
 		{
 			if (item == val)
-				return;
+				return false;
 		}
 
 		vector.push_back(val);
+		return true;
 	}
 
 
 	template<typename T>
-	void removeFromVector(std::vector<T *> & vector, T * val) // removes an element from a vector if it is present. This is placed in the header because it is a template function.
+	bool removeFromVector(std::vector<T*>& vector, T* val) // removes an element from a vector if it is present. This is placed in the header because it is a template function.
 	{
 		for (int i = 0; i < vector.size(); i++)
 		{
 			if (vector.at(i) == val) // if the pointer has been found, it is removed.
 			{
 				vector.erase(vector.begin() + i);
-				return;
+				return true;
 			}
 		}
+
+		return false;
 	}
 
 
